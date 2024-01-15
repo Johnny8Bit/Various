5c4be92d83e6d8b2fc111ee84dd7f8398cb891be
Michał Kowalik (kowalikm) <kowalikm@cisco.com>
2024-01-15 11:02:29 +0100

    added CSV merge script

---
+++ csvmerge.py
@@ -0,0 +1,34 @@
+'''
+Merges all CSV files in current woring directory into single CSV file
+
+'''
+import os
+import csv
+
+FILE_EXTENSION = ".csv"
+NEW_FILE = "merged.csv"
+
+def read_all_csv(): 
+
+    data = []
+    files = (file for file in os.listdir(os.getcwd()) if file.endswith(FILE_EXTENSION))
+    for file in files:
+
+        with open(file, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                data.append(row)
+
+    return data
+
+def write_new_csv(data):
+
+    with open(NEW_FILE, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for row in data:
+            writer.writerow(row)
+
+
+if __name__ == '__main__':
+
+    write_new_csv(read_all_csv())
